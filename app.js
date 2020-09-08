ab030ac85553d8dd0e0b61873783fb3725895624
Ogtsn99 <fantasista4123@icloud.com>
2020-09-09 01:30:41 +0900

    req.userが機能した

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ const logger = require('morgan');
 const session = require('express-session');
 const passport = require('passport');
 const TwitterStrategy = require('passport-twitter').Strategy;
+const cors = require('cors')
 require('dotenv').config();
+
+
+const TWITTER_CONSUMER_KEY = process.env.TWITTER_CONSUMER_KEY
+const TWITTER_CONSUMER_SECRET = process.env.TWITTER_CONSUMER_SECRET
+const API_ROOT = process.env.API_ROOT || 'http://127.0.0.1:3000'
+const CLIENT_ROOT = process.env.CLIENT_ROOT || 'http://localhost:4000/'
+
+console.log("api -> ", API_ROOT)
+console.log("client ->", CLIENT_ROOT)
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -22,19 +32,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-const TWITTER_CONSUMER_KEY = process.env.TWITTER_CONSUMER_KEY
-const TWITTER_CONSUMER_SECRET = process.env.TWITTER_CONSUMER_SECRET
-const API_ROOT = process.env.API_ROOT || 'http://127.0.0.1:3000'
-const CLIENT_ROOT = process.env.CLIENT_ROOT || 'http://localhost:4000/'
-
-console.log("api -> ", API_ROOT)
-console.log("client ->", CLIENT_ROOT)
+app.use(
+  cors({
+    origin: CLIENT_ROOT,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true
+  })
+);
 
 // ユーザ情報をセッションに保存するので初期化
 app.use(session({
   secret: 'secret-key',
-  resave: true,
+  resave: false,
   saveUninitialized: false,
   HttpOnly: false,
   Domain: CLIENT_ROOT
@@ -63,6 +72,8 @@ passport.use(new TwitterStrategy({
     return done(null, profile);
   }
 ));
+
+app.use(passport.session());
 
 app.get('/auth/twitter', passport.authenticate('twitter'));
 app.get('/auth/twitter/callback',
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req.user)
+  if(req.user) res.json({state: "ログイン中"})
+  else res.json({state: "ログアウト中"})
 });
 
 module.exports = router;
