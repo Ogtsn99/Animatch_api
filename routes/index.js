77dc714ff5c8baca65e04843a155b1508fb0c224
Ogtsn99 <fantasista4123@icloud.com>
2020-09-09 17:34:23 +0900

    authCheckにres.headerをセットしてみる。だめもと

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const authCheck = (req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   if (!req.user) {
     res.status(401).json({
       authenticated: false,
